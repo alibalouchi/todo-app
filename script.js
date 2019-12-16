f71bb0b74d9b10facfb0fd885905e7167d5b7288
Ali Balochi <Ali.Balochi@MahanAir.Aero>
2019-12-16 15:40:48 +0330

    fix filter-buttons fix complete checkbox add some css

--- script.js
+++ script.js
@@ -13,13 +13,17 @@ const addTodo = () => {
 }
 
 const showTodos = (todoArray) => {
-	let removeButton = "<button onclick='removeTodo(event)' class='remove-button'>remove</button>";
-	let editButton = "<button onclick='editTodo(event)' class='edit-button'>edit</button>";
-	let completeCheckbox = "";
+	let removeButton = "<button onclick='removeTodo(event)' class='button'>remove</button>";
+	let editButton = "<button onclick='editTodo(event)' class='button'>edit</button>";
 	let table = document.getElementById("addedTodos");
 	table.innerHTML = ""
 	todoArray.map(item => {
-		table.innerHTML += `<tr id=${item.id}><td>${removeButton}</td><td>${item.todoTitle}</td><td>${editButton}</td><td><input onclick='clickCheckbox(event)' type='checkbox' class='complete-checkbox' /></td></tr>`
+		table.innerHTML += `<tr ${item.complete ? 'class = complete' : ''} id=${item.id}>
+			<td>${removeButton}</td>
+			<td>${item.todoTitle}</td>
+			<td>${editButton}</td>
+			<td><input ${item.complete ? 'checked' : ''} onclick='clickCheckbox(event)' type='checkbox' /></td>
+			</tr>`
 	})
 }
 
@@ -45,5 +49,24 @@ const removeTodo = (event) => {
 const clickCheckbox = (event) => {
 	let id = event.path[2].id;
 	let checkedItem = todos.filter(item => item.id == id)[0]
+
 	checkedItem.complete = !checkedItem.complete
+	showTodos(todos)
+}
+
+const handleShow = () => {
+	let showObj = {
+		complete: document.getElementById("complete").checked,
+		active: document.getElementById("active").checked,
+		all: document.getElementById("all").checked
+	};
+	if (showObj.complete){
+		let showTodo = todos.filter(item => item.complete == true);
+		showTodos(showTodo);
+	}else if (showObj.active){
+		let showTodo = todos.filter(item => item.complete == false);
+		showTodos(showTodo);
+	}else if (showObj.all){
+		showTodos(todos);
+	}
 }
